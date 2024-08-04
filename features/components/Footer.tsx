bdea138df9cf89d0e4d424ac71bf84a9d8242067
Tosapon Waikitja <tonspu55@gmail.com>
2024-08-05 02:17:55 +0700

    update icon facebook

--- features/components/Footer.tsx
+++ features/components/Footer.tsx
@@ -4,17 +4,14 @@ import type { IconProps } from "@iconify/react";
 
 import React from "react";
 import { Link, Spacer } from "@nextui-org/react";
-import { Icon } from "@iconify/react";
+import Image from "next/image";
 
 type SocialIconProps = Omit<IconProps, "icon">;
-
 const socialItems = [
   {
-    name: "Facebook",
+    alt: "facebook",
     href: "https://www.facebook.com/profile.php?id=61552595019036&mibextid=ZbWKwL",
-    icon: (props: SocialIconProps) => (
-      <Icon {...props} icon="fontisto:facebook" />
-    ),
+    src: "/assets/images/facebook.jpeg",
   },
 ];
 
@@ -33,15 +30,14 @@ const Footer = () => {
         </div> */}
 
         <div className="flex justify-center mt-5 gap-x-4">
-          {socialItems.map((item) => (
-            <Link
-              key={item.name}
-              isExternal
-              className="text-default-400"
-              href={item.href}
-            >
-              <span className="sr-only">{item.name}</span>
-              <item.icon aria-hidden="true" className="w-5" />
+          {socialItems.map((item, index) => (
+            <Link key={index} isExternal href={item.href}>
+              <Image
+                src={item.src}
+                alt={item.alt}
+                width={50}
+                height={50}
+              ></Image>
             </Link>
           ))}
         </div>
